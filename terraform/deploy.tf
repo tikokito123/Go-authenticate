263aee88961a2f71bf11017bf5a5ed49be0a7fae
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2021-10-24 07:24:10 +0300

    blue\green implementation

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -42,7 +42,6 @@ module "vpc" {
 
 }
 
-
 resource "aws_security_group" "firewall" {
   vpc_id = module.vpc.vpc_id
 
@@ -69,29 +68,53 @@ resource "aws_security_group" "firewall" {
 
 }
 
+// WARNING! billing!
 
-resource "aws_launch_template" "template" {
-  name_prefix            = "instance-auto-scalling"
-  image_id               = var.ami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.firewall.id]
-  user_data              = filebase64("./run-docker.sh")
+// resource "aws_launch_template" "template" {
+//   name_prefix            = "instance-auto-scalling"
+//   image_id               = var.ami
+//   instance_type          = "t2.micro"
+//   vpc_security_group_ids = [aws_security_group.firewall.id]
+//   user_data              = filebase64("./run-docker.sh")
+// }
+
+
+
+
+
+resource "aws_launch_configuration" "web" {
+  name_prefix = "web-"
+
+  image_id      = var.ami
+  instance_type = "t2.micro"
+
+  security_groups             = [aws_security_group.firewall.id]
+  associate_public_ip_address = true
+
+  user_data = "${filebase64("./run-docker.sh")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "auto-scalling" {
-  vpc_zone_identifier = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
-  desired_capacity    = 2
-  max_size            = 3
-  min_size            = 1
-
-  health_check_type = "ELB"
+  vpc_zone_identifier  = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  launch_configuration = aws_launch_configuration.web.name
+  health_check_type    = "ELB"
 
   load_balancers = [
     aws_elb.web_elb.id
   ]
-
-  launch_template {
-    id = aws_launch_template.template.id
+  // launch_template will trigger your credit card
+  // launch_template {
+  //   id = aws_launch_template.template.id
+  // }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
