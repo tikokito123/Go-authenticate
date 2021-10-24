f5f8f7a77d1c53f2fcabfff74c75fed040d86126
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2021-10-24 06:19:41 +0300

    added auto scalling

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -21,32 +21,32 @@ provider "aws" {
 }
 
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+// resource "tls_private_key" "private_key" {
+//   algorithm = "RSA"
+//   rsa_bits  = 4096
+// }
 
-resource "aws_key_pair" "key" {
-  key_name   = "keyPair"
-  public_key = tls_private_key.private_key.public_key_openssh
-}
+// resource "aws_key_pair" "key" {
+//   key_name   = "keyPair"
+//   public_key = tls_private_key.private_key.public_key_openssh
+// }
 
-resource "aws_instance" "app_server" {
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  count                  = 2
-  subnet_id              = element(module.vpc.public_subnets, count.index)
-  key_name               = aws_key_pair.key.key_name
-  user_data              = file("./run-docker.sh")
-  vpc_security_group_ids = ["${aws_security_group.firewall.id}"]
+// resource "aws_instance" "app_server" {
+//   ami                    = var.ami
+//   instance_type          = "t2.micro"
+//   count                  = 2
+//   subnet_id              = element(module.vpc.public_subnets, count.index)
+//   key_name               = aws_key_pair.key.key_name
+//   user_data              = file("./run-docker.sh")
+//   vpc_security_group_ids = ["${aws_security_group.firewall.id}"]
 
-  tags = {
-    Name      = "TikalTestServerInstance"
-    Terraform = "true"
-    ENV       = var.env
-  }
+//   tags = {
+//     Name      = "TikalTestServerInstance"
+//     Terraform = "true"
+//     ENV       = var.env
+//   }
 
-}
+// }
 
 
 module "vpc" {
@@ -87,57 +87,117 @@ resource "aws_security_group" "firewall" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   // this is here only for testing! do not use it in production!!!
-  // ingress {
-  //   from_port   = 22
-  //   to_port     = 22
-  //   protocol    = "tcp"
-  //   cidr_blocks = ["0.0.0.0/0"]
-  // }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
 
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-  name    = "tikal-mission-alb"
 
-  load_balancer_type = "application"
+resource "aws_launch_template" "template" {
+  name_prefix            = "instance-auto-scalling"
+  image_id               = var.ami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.firewall.id]
+  user_data              = filebase64("./run-docker.sh")
+}
 
-  vpc_id          = module.vpc.vpc_id
-  subnets         = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
-  security_groups = [aws_security_group.firewall.id]
+resource "aws_autoscaling_group" "auto-scalling" {
+  vpc_zone_identifier = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
 
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = [
-        {
-          target_id = "${aws_instance.app_server[0].id}"
-          port      = 80
-        },
-        {
-          target_id = "${aws_instance.app_server[1].id}"
-          port      = 80
-        }
-      ]
-    }
+  health_check_type = "ELB"
+
+  load_balancers = [
+    aws_elb.web_elb.id
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = var.env
-    terraform   = "true"
+  launch_template {
+    id = aws_launch_template.template.id
   }
 }
+
+
+resource "aws_elb" "web_elb" {
+  name = "web-elb"
+  security_groups = [
+    aws_security_group.firewall.id
+  ]
+  subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+
+  cross_zone_load_balancing = true
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
+  }
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "80"
+    instance_protocol = "http"
+  }
+
+  tags = {
+    Name      = "web_ELB"
+    terraform = "True"
+  }
+
+}
+
+
+
+// module "alb" {
+//   source  = "terraform-aws-modules/alb/aws"
+//   version = "~> 6.0"
+//   name    = "tikal-mission-alb"
+
+//   load_balancer_type = "application"
+
+//   vpc_id          = module.vpc.vpc_id
+//   subnets         = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+//   security_groups = [aws_security_group.firewall.id]
+
+//   target_groups = [
+//     {
+//       name_prefix      = "pref-"
+//       backend_protocol = "HTTP"
+//       backend_port     = 80
+//       target_type      = "instance"
+//       targets = [
+//         {
+//           target_id = "${aws_instance.app_server[0].id}"
+//           port      = 80
+//         },
+//         {
+//           target_id = "${aws_instance.app_server[1].id}"
+//           port      = 80
+//         }
+//       ]
+//     }
+//   ]
+
+//   http_tcp_listeners = [
+//     {
+//       port               = 80
+//       protocol           = "HTTP"
+//       target_group_index = 0
+//     }
+//   ]
+
+//   tags = {
+//     Environment = var.env
+//     terraform   = "true"
+//   }
+// }
 
 
