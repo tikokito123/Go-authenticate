22d291f5b7478e79387f4546f8fda0abf275a518
Tikokito <30938070+tikokito123@users.noreply.github.com>
2021-10-31 00:34:19 +0300

    front-end design

---
+++ client/src/Components/SideBar.jsx
@@ -0,0 +1,47 @@
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarContent,
+  SidebarFooter,
+} from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+
+export default function SideBar() {
+  return (
+    <ProSidebar
+      className=""
+      id="sidebar"
+      toggled={true}
+      breakPoint="xs"
+    >
+      <SidebarHeader
+        className="text-light font-weight-bold p-5 d-flex"
+        style={{ fontSize: "30px" }}
+      >
+        <a
+          href="/"
+          className=""
+          style={{ color: "inherit", textDecoration: "none" }}
+        >
+          TikoWeb
+        </a>
+      </SidebarHeader>
+
+        <Menu iconShape="square">
+          <MenuItem icon="">Our Music</MenuItem>
+          <MenuItem icon="">What can we do?</MenuItem>
+          <MenuItem icon="">About Us</MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      <SidebarFooter>
+        Here is the footer
+      </SidebarFooter>
+    </ProSidebar>
+  );
+}
